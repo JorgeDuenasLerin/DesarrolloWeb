f6373cbb9de5a1758d4900443c76f2dd41d9d751
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-01-26 14:01:36 +0100

    Tarea con vistas

--- IAW/03_DjangoTutorial/ahorcado/polls/views.py
+++ IAW/03_DjangoTutorial/ahorcado/polls/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hola mundo del desarrollo django.")
+    preguntas = Question.objects.all()
+    """
+    Tarea/Reto
+    1º Saca esta información como si fuera una lista HTML (ol-li)
+    2º Saca de cada pregunta sus opciones
+    """
+    html = "Hola mundo del desarrollo django.<br>"
+    for p in preguntas:
+        html = html + p.question_text + "<br>"
+    return HttpResponse(html)
+
+
+def detail(request, question_id):
+    return HttpResponse("Este es el detalle de la pregunta con id: %s." % question_id)
+
+def results(request, question_id):
+    response = "Estás en el resultado de la pregunta con id %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Votación para " + str(question_id))
